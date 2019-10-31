ae43203cab9d21f2f44a9dad1268797f4977f4ac
victorpena13 <victor.pena1392@gmail.com>
2019-10-31 14:34:17 -0500

    completed if exercise

---
+++ arrays.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = array('Joe', 'Blow');
+
+var_dump($name);
+
+var_dump($name[0]);
+var_dump($name[1]);
+
+print_r($name);
+
+echo $name[1];
+
+
+
+//Create a variable $test and make it a multidimensional array with the keys
+//person1, person2, and person3 with your person array from last step, and 2
+//more people arrays, as values
---
+++ if.php
@@ -0,0 +1,49 @@
+<?php
+
+$a = 5;
+$b = 10;
+$c = '10';
+
+if ($a < $b) {
+    // TODO: Replace the blanks with the correct description
+    echo "$a is less than $b\n";
+}
+
+if ($b > $a) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is greater than $a\n";
+}
+
+if ($b >= $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is greater than or equal to $c\n";
+}
+
+if ($b <= $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is less than or equal to $c\n";
+}
+
+if ($b == $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is equal after type juggling $c\n";
+}
+
+if ($b === $c) {
+    // TODO: Replace the blanks with the correct description
+    echo "$b is identical $c\n";
+}
+
+// TODO: Replace `true` with the correct comparison
+if ($b != $c) {
+    echo "$b is not equal to $c\n after type juggling ";
+} else if ($b === $c) {
+    echo "$b is identical $c\n";
+}
+
+// TODO: Replace `true` with the correct comparison
+if ($b !== $c) {
+    echo "$b is not identical to $c\n";
+} else if ($b == $c) {
+    echo "$b is equal to $c\n";
+}
