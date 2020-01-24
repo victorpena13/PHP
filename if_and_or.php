11a91a6cda187d06ed71df4282849b244de5647b
victorpena13 <victor.pena1392@gmail.com>
2020-01-24 15:51:01 -0600

    an if statement that checks if the value of x is this OR that

--- if_and_or.php
+++ if_and_or.php
@@ -12,6 +12,10 @@ echo "{$x}  > {$y}  >  {$z}\n";
 // If 0 is less than $x OR $x is less than 10
 // then echo the result as a sentence "0 is less than {$x} OR {$x} is less than 10".
 
+if (0  < $x || $x < 10) {
+    echo "0 is less than {$x} OR {$x} is less than 10\n";
+}
+
 // TODO:
 // repeat the if statement for $y and $z.
 
