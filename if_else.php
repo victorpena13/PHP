40e70ca1de61fd7095b6ae1fe63d97f6229b171f
victorpena13 <victor.pena1392@gmail.com>
2020-01-26 13:35:14 -0600

    as long as a is less then b it will read the first echo statement.     if a equals ten or more it will display the second echo statement.

--- if_else.php
+++ if_else.php
@@ -5,11 +5,42 @@ $b = 10;
 $c = '10';
 
 // TODO: Shorten these two if statements to a single if/else
+<?php
+
+$a = 5;
+$b = 10;
+$c = '10';
+
+// TODO: Shorten these two if statements to a single if/else
 if ($a < $b) {
     echo "$a is less than $b\n";
-}
-if ($a >= $b) {
+} else {
     echo "$a is greater than or equal to $b\n";
+}
+
+//as long as a is less then b it will read the first echo statement.
+//    if a equals ten or more it will display the second echo statement.
+
+// TODO: Shorten these two if statements to a single if/else
+if ($b < $c) {
+    echo "$b is less than $c\n";
+}
+if ($b >= $c) {
+    echo "$b is greater than or equal to $c\n";
+}
+
+// TODO:
+// combine the next 3 conditionals into one
+// if/elseif/else block that checks in order for:
+// identical, equal, not equal/identical
+if ($b === $c) {
+    echo "$b is identical to $c\n";
+}
+if ($b == $c) {
+    echo "$b is equal to $c\n";
+}
+if ($b != $c) {
+    echo "$b is not equal to $c\n";
 }
 
 // TODO: Shorten these two if statements to a single if/else
