1c3fac456c749068abb0a62cf8d98703dbff9ab0
victorpena13 <victor.pena1392@gmail.com>
2020-01-26 15:15:07 -0600

    fixed comments

--- do-while.php
+++ do-while.php
@@ -1,24 +1,26 @@
 <?php
 
-//$two = -1;
-//do {
-//    $two++;
-//    if ($two % 2 == 0) {
-//        echo $two . "\n";
-//    }
-//} while ($two <= 100);
-//
-//
-//$oneHundred = 105;
-//
-//do {
-//    $oneHundred--;
-//    if ($oneHundred % 5 == 0) {
-//        echo $oneHundred . "\n";
-//    }
-//} while ($oneHundred >= -10);
 
+//exercise 1
+$two = -1;
+do {
+    $two++;
+    if ($two % 2 == 0) {
+        echo $two . "\n";
+    }
+} while ($two <= 100);
 
+//exercise 2
+$oneHundred = 105;
+
+do {
+    $oneHundred--;
+    if ($oneHundred % 5 == 0) {
+        echo $oneHundred . "\n";
+    }
+} while ($oneHundred >= -10);
+
+//exercise 3
 $a = 2;
 
 do {
