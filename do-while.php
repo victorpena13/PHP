75e6103b43f5afac06d2caff2d7da24f2a636c99
victorpena13 <victor.pena1392@gmail.com>
2020-01-26 14:56:56 -0600

    do-while loop to count backwards by 5's from 100 to -10.

--- do-while.php
+++ do-while.php
@@ -9,3 +9,15 @@ do {
         echo "";
     }
 } while ($two <= 100);
+
+
+$oneHundred = 105;
+
+do {
+    $oneHundred--;
+    if ($oneHundred % 5 == 0) {
+        echo $oneHundred . "\n";
+    } else {
+        echo "";
+    }
+} while ($oneHundred >= -10);
