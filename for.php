1d53f042bd3abfdb3d9f56a2b40fde2cb78d9eab
victorpena13 <victor.pena1392@gmail.com>
2020-01-28 16:09:53 -0600

    alerts user if they did not enter a number

--- for.php
+++ for.php
@@ -1,14 +1,36 @@
 <?php
 
-fwrite(STDOUT, 'enter first value: ');
-$firstNum = fgets(STDIN);
+fwrite(STDOUT, 'press enter to begin ');
+$play = fgets(STDIN);
 
-fwrite(STDOUT, 'enter second value: ');
-$secondNum = fgets(STDIN);
+    fwrite(STDOUT, 'enter a starting number: ');
+    $firstNum = fgets(STDIN);
+    if (is_numeric($firstNum)) {
+        fwrite(STDOUT, 'enter second value: ');
+        $secondNum = fgets(STDIN);
+    } else {
+        echo "That is not a number\n";
+    }
 
-fwrite(STDOUT, 'Enter an amount to increment by: ');
-$increment = fgets(STDIN);
+    if (is_numeric($secondNum)) {
+        fwrite(STDOUT, 'Enter an amount to increment by: ');
+        $increment = fgets(STDIN);
+    } else {
+        echo "That is not a number\n";
+    }
 
-for ($a = $firstNum; $a <= $secondNum; $a+=$increment) {
-    echo $a . "\n";
+    if (is_numeric($increment)) {
+        for ($a = $firstNum; $a <= $secondNum; $a+=$increment) {
+            echo $a . "\n";
+        }
 }
+
+
+
+
+
+
+
+
+
+
