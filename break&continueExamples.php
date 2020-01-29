55a201eea9d053ff24f1a11039a6e15b34e9849e
victorpena13 <victor.pena1392@gmail.com>
2020-01-29 13:27:26 -0600

    another example

--- break&continueExamples.php
+++ break&continueExamples.php
@@ -14,3 +14,17 @@ for ($i = 1; $i <= 10; $i++) {
     }
     echo "^ that is an odd number";
 }
+
+$numbers = array(1, 2, 3, 4, 5);
+// Loop 1
+foreach ($numbers as $key => $value) {
+    echo "{$value}\n";
+    // Loop 2
+    for ($i = 1; $i <= 10; $i++) {
+        if ($i == 2) {
+            echo "{$i}\n";
+            break 2;
+        }
+    }
+}
+echo "done!\n";
