da737f9a8db1777ff70a9735d320ae1494386d0e
victorpena13 <victor.pena1392@gmail.com>
2020-01-28 19:40:21 -0600

    for each and key value examples

--- forEachKeys.php
+++ forEachKeys.php
@@ -14,3 +14,20 @@ $studentDetails = array(
 foreach ($studentDetails as $key => $value) {
     echo "Student's $key is $value\n";
 }
+
+$students = array(
+    array('name' => 'Virginia Potts', 'age' => 29),
+    array('name' => 'Elon Musk', 'age' => 42),
+    array('name' => 'Rasmus Lerdorf', 'age' => 45),
+    array('name' => 'Marissa Mayer', 'age' => 38)
+);
+
+foreach ($students as $student) {
+    echo "{$student['name']} is {$student['age']} years old.\n";
+}
+
+foreach ($students as $student) {
+    foreach ($student as $key => $value) {
+        echo "Student's $key is $value\n";
+    }
+}
