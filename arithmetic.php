318b978c5b2da8dede7ee74a75a9abb8b808d0c1
victorpena13 <victor.pena1392@gmail.com>
2020-01-29 15:23:13 -0600

    functions exercise

--- arithmetic.php
+++ arithmetic.php
@@ -2,22 +2,38 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+    echo $a + $b . "\n";
 }
 
 function subtract($a, $b)
 {
-    // Add code here
+    echo $a - $b . "\n";
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+   echo $a * $b . "\n";
 }
 
 function divide($a, $b)
 {
-    // Add code here
+    echo $a / $b . "\n";
 }
 
 // Add code to test your functions here
+
+fwrite(STDOUT, 'enter first value ');
+$firstValue = fgets(STDIN);
+
+fwrite(STDOUT, 'enter second value ');
+$secondValue = fgets(STDIN);
+
+
+
+add($firstValue, $secondValue);
+subtract($firstValue, $secondValue);
+multiply($firstValue, $secondValue);
+divide($firstValue, $secondValue);
+
+
+
