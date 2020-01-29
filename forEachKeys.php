a4aea7bf21e05bb3f07ad78b2b0cda9a70aff9d7
victorpena13 <victor.pena1392@gmail.com>
2020-01-28 19:37:05 -0600

    added a line break on line 15

--- forEachKeys.php
+++ forEachKeys.php
@@ -5,3 +5,12 @@ $numbers = [1, 2, 3, 4, 5];
 foreach ($numbers as $key => $value) {
     echo "\$value has a key of $key and a value of $value\n";
 }
+
+$studentDetails = array(
+    'name' => 'Jane Doe',
+    'age' => 29
+);
+
+foreach ($studentDetails as $key => $value) {
+    echo "Student's $key is $value\n";
+}
