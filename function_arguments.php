45e7b4224637347f5d8cd9c5330f2434521b9ea0
victorpena13 <victor.pena1392@gmail.com>
2020-02-01 21:52:37 -0600

    using function arguments

--- function_arguments.php
+++ function_arguments.php
@@ -19,3 +19,39 @@ echo compare('10', 10, true);
 
 echo compare('10', 10, false);
 //results in true.
+
+
+
+
+
+function inspect($variable = null, $dump = true) {
+    // Same as if ($dump == true) {...}
+    if ($dump) {
+        var_dump($variable);
+    } else {
+        print_r($variable);
+    }
+}
+
+//Testing this in the interactive shell lets us see the results.
+//
+//php > inspect();
+//NULL
+//
+//php > inspect(array(1,2,3), false);
+//Array
+//(
+//    [0] => 1
+//    [1] => 2
+//    [2] => 3
+//)
+//
+//php > inspect(array(1,2,3));
+//array(3) {
+//    [0]=>
+//  int(1)
+//  [1]=>
+//  int(2)
+//  [2]=>
+//  int(3)
+//}
