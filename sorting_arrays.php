a93a47fcb3517a503d89356327e60c177e84249f
victorpena13 <victor.pena1392@gmail.com>
2020-02-05 16:18:47 -0600

    sorting arrays exercises

--- sorting_arrays.php
+++ sorting_arrays.php
@@ -2,6 +2,9 @@
 
 $names = ['Marc Andreessen', 'Tim Berners-Lee', 'Len Bosack', 'Steve Case', 'Vint Cerf', 'Len Kleinrock', 'J.C.R. Licklider', 'Bob Metcalfe', 'Ray Tomlinson'];
 
-asort($names);
+//ksort($names);
+//print_r($names);
+
+sort($names);
 print_r($names);
 //prints the array from a - z
