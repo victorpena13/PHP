eb0756906e9636ac5ec5b1b04ad48befdfd520db
victorpena13 <victor.pena1392@gmail.com>
2020-02-05 14:57:57 -0600

    create a new function called inspect() that takes in one argument. Your inspect() function should look at the passed argument and return a string with the variable's type and its value

--- internal_functions.php
+++ internal_functions.php
@@ -2,6 +2,28 @@
 
 // TODO: Create your inspect() function here
 
+function inspect($a) {
+   switch(gettype($a)) {
+       case is_string($a):
+           echo "$a is a string\n";
+           break;
+       case is_array($a):
+           echo "$a is an array\n";
+           break;
+       case is_bool($a):
+           echo "$a is a bool\n";
+           break;
+       case is_null($a):
+           echo "$a is null\n";
+           break;
+       case is_numeric($a):
+           echo "$a is numeric\n";
+           break;
+       default:
+           echo "not recognizable\n";
+   }
+}
+
 // Do not mofify these variables!
 $string1 = "I'm a little teapot";
 $string2 = '';
@@ -18,23 +40,34 @@ $null = NULL;
 // TODO: After each echo statement, use inspect() to output the variable's type and its value
 
 echo 'Inspecting $num1:' . PHP_EOL;
+inspect($num1);
 
 echo 'Inspecting $num2:' . PHP_EOL;
+inspect($num2);
 
 echo 'Inspecting $num3:' . PHP_EOL;
+inspect($num3);
 
 echo 'Inspecting $num4:' . PHP_EOL;
+inspect($num4);
 
 echo 'Inspecting $null:' . PHP_EOL;
+inspect($null);
 
 echo 'Inspecting $bool1' . PHP_EOL;
+inspect($bool1);
 
 echo 'Inspecting $bool2' . PHP_EOL;
+inspect($bool2);
 
 echo 'Inspecting $string1' . PHP_EOL;
+inspect($string1);
 
 echo 'Inspecting $string2' . PHP_EOL;
+inspect($string2);
 
 echo 'Inspecting $array1' . PHP_EOL;
+inspect($array1);
 
 echo 'Inspecting $array2' . PHP_EOL;
+inspect($array2);
