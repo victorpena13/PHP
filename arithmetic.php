e3913660467fdf370b2c6d42141eeab5f4660c8a
victorpena13 <victor.pena1392@gmail.com>
2020-02-05 14:25:03 -0600

    Refactor the error messages into their own throwErrorMessage() function, have the other functions use it for error messaging.

--- arithmetic.php
+++ arithmetic.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$firstValue = "0";
+$firstValue = "fun";
 $secondValue = 3;
 
 
@@ -10,12 +10,24 @@ if(is_numeric($firstValue) && is_numeric($secondValue)) {
     subtract($firstValue, $secondValue);
     multiply($firstValue, $secondValue);
     divide($firstValue, $secondValue);
-} else {
-    echo "Enter a numeric value.\n you entered: $a, $b\n";
+}
+
+function throwErrorMessages($a, $b, $division = false) {
+    if(!is_numeric($a)) {
+    fwrite(STDERR, "$a should be a number.\n");
+    }
+    if(!is_numeric($b)) {
+        fwrite(STDERR, "$b should be a number");
+    }
+    if($division == true) {
+        if ($a == 0 || $b == 0) {
+            fwrite(STDERR, "cannot divide by $b or $a");
+        }
+    }
 }
 
 
-
+throwErrorMessages($firstValue, $secondValue, false);
 function add($a, $b) {
     echo $a + $b . "\n";
 }
@@ -31,11 +43,9 @@ function multiply($a, $b)
 
 function divide($a, $b)
 {
-    if($a == 0 || $b == 0) {
-        echo "You cannot divide by $a and $b\n";
-    } else {
+
         echo $a / $b . "\n";
-    }
+
 }
 
 
