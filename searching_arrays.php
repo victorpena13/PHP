471b544ea9205f6e6fce5dd24d0ba5ff9b1f33fb
victorpena13 <victor.pena1392@gmail.com>
2020-02-06 18:11:47 -0600

    examples of searching arrays

---
+++ searching_arrays.php
@@ -0,0 +1,22 @@
+<?php
+
+$names = ['Marc Andreessen', 'Tim Berners-Lee', 'Len Bosack', 'Steve Case', 'Vint Cerf', 'Len Kleinrock', 'J.C.R. Licklider', 'Bob Metcalfe', 'Ray Tomlinson'];
+
+
+$result = array_search('Bill Gates', $names);
+var_dump($result . "\n");
+//result: bool(false)
+
+$resultTwo = array_search('Bob Metcalfe', $names);
+var_dump($resultTwo);
+//result: int(7)
+
+$query = 'Tim Berners-Lee';
+
+$resultThree = array_search($query, $names);
+
+if($resultThree) {
+    echo $names[$resultThree . "\n"];
+}
+//result: Tim Berners-Lee
+
