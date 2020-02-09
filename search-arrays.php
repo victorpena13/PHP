52386644d8316c482347a79b52d6f7527cff30d4
victorpena13 <victor.pena1392@gmail.com>
2020-02-08 18:15:33 -0600

    last example

--- search-arrays.php
+++ search-arrays.php
@@ -3,3 +3,11 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+
+$query = "Tina";
+$result = array_search($query, $names);
+
+if ($result) {
+    echo $names[$result] . "\n";
+}
--- searching_arrays.php
+++ searching_arrays.php
@@ -20,3 +20,15 @@ if($resultThree) {
 }
 //result: Tim Berners-Lee
 
+
+$names = ['Marc Andreessen', 'Tim Berners-Lee', 'Len Bosack', 'Steve Case', 'Vint Cerf', 'Len Kleinrock', 'J.C.R. Licklider', 'Bob Metcalfe', 'Ray Tomlinson'];
+
+$query = 'Tim Berners-Lee';
+
+$result = array_search($query, $names);
+
+if ($result) {
+    echo $names[$result];
+}
+
+//result: Time Berners-Lee
