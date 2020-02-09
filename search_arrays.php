30830d9d9b20275fa31b6435232c792c6777e95c
victorpena13 <victor.pena1392@gmail.com>
2020-02-08 23:39:03 -0600

    gives the answers that are not matching in the first array

--- search_arrays.php
+++ search_arrays.php
@@ -7,6 +7,9 @@ $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 $query = 'Dana';
 $result = array_search($query, $names);
 
+//Create a function that returns TRUE or FALSE if an
+//array value is found. Search for Tina and Bob in
+//$names. Make sure it works as expected.
 
 function hasValue($result) {
     if ($result >=0 && $result <= 4) {
@@ -18,4 +21,21 @@ function hasValue($result) {
 }
 
 hasValue($result);
+
+//Create a function to compare 2 arrays that returns the
+//number of values in common between the arrays.
+//Use the 2 example arrays and make sure your solution
+//uses array_search().
+
+
+$answer = array_diff($names, $compare);
+print_r($answer);
+//result:
+//Array
+//(
+//    [1] => Dana
+//    [2] => Mike
+//    [4] => Adam
+//)
+
 
