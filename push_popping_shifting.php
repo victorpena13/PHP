73fb1266de431bba5cc63829768f999c3dbcf6e3
victorpena13 <victor.pena1392@gmail.com>
2020-02-10 18:03:44 -0600

    used array_push to add 3 values to the  array

---
+++ push_popping_shifting.php
@@ -0,0 +1,15 @@
+<?php
+
+$items = ['First', 'Second', 'Third'];
+array_push($items, 'Fourth', 'Fifth', 'Sixth');
+print_r($items);
+//result:
+//Array
+//(
+//    [0] => First
+//    [1] => Second
+//[2] => Third
+//[3] => Fourth
+//[4] => Fifth
+//[5] => Sixth
+//)
