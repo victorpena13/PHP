a55e57841b3a35c224b9f9c2327dc5dfc2eec9e2
victorpena13 <victor.pena1392@gmail.com>
2020-02-10 19:46:00 -0600

    implode() example

---
+++ implode.php
@@ -0,0 +1,16 @@
+<?php
+// implode()
+// takes an array and combines the values into a string.
+// implode() syntax:
+// string implode ( string $glue, array $pieces )
+
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$physicistsArray = explode (', ', $physicistsString);
+
+//print_r($physicistsArray);
+
+$physicistsStrng = implode('|', $physicistsArray);
+echo $physicistsStrng . "\n";
+
+// result:
+// ordon Freeman|Samantha Carter|Sheldon Cooper|Quinn Mallory|Bruce Banner|Tony Stark
