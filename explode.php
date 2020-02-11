dd2b4ff818b23232cd80a5649fd2e1499bb6443c
victorpena13 <victor.pena1392@gmail.com>
2020-02-10 19:37:25 -0600

    explode() example

--- explode.php
+++ explode.php
@@ -2,5 +2,10 @@
 //explode()
 //this converts a string into an array.
 // explode format:
-// array explode ( string $delimiter, string $string [, int $limit ] )
+// $array = explode ( string $delimiter, string $string [, int $limit ] )
 
+
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$physicistsArray = explode (', ', $physicistsString);
+
+print_r($physicistsArray);
