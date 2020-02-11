5473a59996391f46fa6e1241d4f49df38c0fa807
victorpena13 <victor.pena1392@gmail.com>
2020-02-10 19:22:12 -0600

    successfully combined names missing into

--- merge-arrays.php
+++ merge-arrays.php
@@ -5,48 +5,26 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 
-
-//Create a function that returns TRUE or FALSE if an
-//array value is found. Search for Tina and Bob in
-//$names. Make sure it works as expected.
-$query = 'Dana';
-$result = array_search($query, $names);
-
-function hasValue($result) {
-    if ($result >=0 && $result <= 4) {
-        echo "TRUE";
-    } else {
-        echo "FALSE";
-    }
-}
-
-hasValue($result);
-
-//Create a function to compare 2 arrays that returns the
-//number of values in common between the arrays.
-//Use the 2 example arrays and make sure your solution
-//uses array_search().
-
-function numberOfValuesInCommon($array1, $array2) {
-    $count = 0;
-
-    foreach($array1 as $value) {
-        if(ifValueExists($value,$array2)) {
-            $count++;
-        }
-    }
-    return $count;
-}
-
-
-echo "The number of values in common is: " . numberOfValuesInCommon($names, $compare) . PHP_EOL;
-
 function combine_arrays($array1, $array2) {
 
     foreach($array1 as $value) {
-        if(!ifValueExists) {
-            array_push($value, $array2);
+        if(!in_array($value, $array2, false)) {
+            array_push( $array2, $value);
         }
     }
+     print_r($array2);
 }
 
+echo "combined arrays: " . combine_arrays($names, $compare);
+//result:
+//Array
+//(
+//    [0] => Tina
+//    [1] => Dean
+//    [2] => Mel
+//    [3] => Amy
+//    [4] => Michael
+//    [5] => Dana
+//    [6] => Mike
+//    [7] => Adam
+//)
