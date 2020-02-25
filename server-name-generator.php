369271928f5eade5608e1b059faefd8030049d92
victorpena13 <victor.pena1392@gmail.com>
2020-02-24 22:52:54 -0600

    created arrays

--- server-name-generator.php
+++ server-name-generator.php
@@ -1,9 +1,5 @@
-<!--We are going to build a server name generator. To do this, we will need to follow the specs below.-->
-<!--Create a new file in your public directory named server-name-generator.php.-->
-<!--Create two arrays.-->
-<!--One containing at least 10 adjectives.-->
-<!--Another containing at least 10 nouns.-->
-<!--Create a function that will return a random element from an array.-->
-<!--Create a function that returns the string value of our new server name made by combining a random adjective with a random noun.-->
-<!--Each time you refresh the page a different result should be displayed.-->
-<!--Make sure you have fun with it. Add CSS to make it fancy.-->
+<?php
+$adjectives = array('dead', 'hairless', 'sadistic', 'metal', 'wild', 'domesticated', 'abnormal', 'medicated');
+
+$nouns = array('factory reset button', 'blood rage', 'toaster', 'legend', 'sandwich', 'kitty');
+
