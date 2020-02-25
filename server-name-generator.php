e1c7181ca4df674641e5daaaae5e87264c44c471
victorpena13 <victor.pena1392@gmail.com>
2020-02-24 23:03:29 -0600

    created function for random adjective

--- server-name-generator.php
+++ server-name-generator.php
@@ -3,3 +3,10 @@ $adjectives = array('dead', 'hairless', 'sadistic', 'metal', 'wild', 'domesticat
 
 $nouns = array('factory reset button', 'blood rage', 'toaster', 'legend', 'sandwich', 'kitty');
 
+
+function randAdjective() {
+    global $adjectives;
+    $random_keys = array_rand($adjectives, 3);
+    echo $adjectives[$random_keys[0]];
+}
+randAdjective();
