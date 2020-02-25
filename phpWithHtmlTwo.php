64af40683e520bfde1e059973f234f8b1bb75dbc
victorpena13 <victor.pena1392@gmail.com>
2020-02-24 22:10:20 -0600

    another way to incorporate html withphp

---
+++ phpWithHtmlTwo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World!</title>
+</head>
+<body>
+<?php
+$message = 'Hello World';
+$showMessage = false;
+if ($showMessage) {
+    echo "<h1>$message</h1>";
+}  else {
+    echo "<h1>Sorry, no message...</h1>";
+}
+?>
+</body>
+</html>
