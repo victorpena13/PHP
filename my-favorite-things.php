0add01b5bb3be631d406abf5be6d1545a06238f6
victorpena13 <victor.pena1392@gmail.com>
2020-02-24 23:43:02 -0600

    looped through favorite things array

--- my-favorite-things.php
+++ my-favorite-things.php
@@ -1,6 +1,19 @@
-<!--Create a new file named my-favorite-things.php and put it in your public directory.-->
-<!--Create an array of your favorite things. This array should contain at least five things.-->
-<!--We will need to loop through this array and display each element inside your HTML.-->
-<!--Use an HTML table for this display.-->
-<!--Use CSS to add a light gray background on every other row for a nice effect.-->
-<!--Have fun and make it beautiful!-->
+<?php
+
+$favoriteThings = array('Tacos', 'Pizza', 'Architecture', 'Software Development', 'Conversation');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Favorite Things</title>
+</head>
+<body>
+<h1>My Fav Things</h1>
+<ol>
+    <?php foreach ($favoriteThings as $favoriteThing) { ?>
+        <li><?php echo $favoriteThing; ?></li>
+    <?php } ?>
+</ol>
+</body>
+</html>
