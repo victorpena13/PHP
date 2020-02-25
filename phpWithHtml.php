8820b65df58599375c27cecd07d3230f3f4b893f
victorpena13 <victor.pena1392@gmail.com>
2020-02-24 22:18:27 -0600

    this example provides the clearest way to incorporate php and html

--- phpWithHtml.php
+++ phpWithHtml.php
@@ -1,14 +1,19 @@
 <?php
 
 $message = 'Hello World';
+$showMessage = false;
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hello World</title>
+    <title>Hello world!</title>
 </head>
 <body>
-<h1><?php echo $message; ?></h1>
+    <?php if ($showMessage) { ?>
+        <h1><?php echo $message; ?></h1>
+    <?php } else { ?>
+        <h1>Sorry, no message...</h1>
+    <?php } ?>
 </body>
 </html>
