a52e4f667a53e98dbb05bd3d763eb894a5eaeb7a
victorpena13 <victor.pena1392@gmail.com>
2020-02-24 23:06:26 -0600

    created random noun function

--- server-name-generator.php
+++ server-name-generator.php
@@ -9,4 +9,13 @@ function randAdjective() {
     $random_keys = array_rand($adjectives, 3);
     echo $adjectives[$random_keys[0]];
 }
+
+function randNoun() {
+    global $nouns;
+    $random_keys = array_rand($nouns, 3);
+    echo $nouns[$random_keys[0]];
+}
+
 randAdjective();
+echo '<br>';
+randNoun();
