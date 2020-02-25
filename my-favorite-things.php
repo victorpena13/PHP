cc7154efab0fb3616741c09d1b7198ffc0a6ee4d
victorpena13 <victor.pena1392@gmail.com>
2020-02-24 23:32:50 -0600

    instructions for second exercise

---
+++ my-favorite-things.php
@@ -0,0 +1,6 @@
+<!--Create a new file named my-favorite-things.php and put it in your public directory.-->
+<!--Create an array of your favorite things. This array should contain at least five things.-->
+<!--We will need to loop through this array and display each element inside your HTML.-->
+<!--Use an HTML table for this display.-->
+<!--Use CSS to add a light gray background on every other row for a nice effect.-->
+<!--Have fun and make it beautiful!-->
