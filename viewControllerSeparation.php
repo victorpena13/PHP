f0a1e92a19b4cbdbf52af172ceac813422e18c82
victorpena13 <victor.pena1392@gmail.com>
2020-03-05 17:43:31 -0600

    passing data with a get request

--- viewControllerSeparation.php
+++ viewControllerSeparation.php
@@ -17,8 +17,17 @@ extract(pageController());
         <title>PHP + HTML</title>
     </head>
     <body>
-    <?php echo $message; ?>
-    <?php echo $test; ?>
+        <?php echo $message; ?>
+
+        <?php echo $test; ?>
+        <br>
+        <a href="http://google.com">Google</a>
+        <br>
+        <form method="GET" action="https://duckduckgo.com/">
+            <input type="text" name="q" value="" placeholder="Search DuckDuckGo">
+            <button type="submit">Go!</button>
+        </form>
+
 
     </body>
 </html>
