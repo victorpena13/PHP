efa81f8e5d6bd58edca29640689df32ae2440927
victorpena13 <victor.pena1392@gmail.com>
2020-03-07 14:56:11 -0600

    created increment and decrement functions. and tested out an evenlistener in the html file.

--- counter.php
+++ counter.php
@@ -6,7 +6,20 @@
 function pageController() {
     //handles all processing for this page.
 
+    $displayedCounter = 1;
+    function increment() {
+        global $displayedCounter;
+        $displayedCounter++;
+    }
 
+    function decrement() {
+        global $displayedCounter;
+        $displayedCounter--;
+    }
+
+    if (empty($displayedCounter)) {
+        $displayedCounter = 1;
+    }
 
 
 
