986d9ff60f699ff9980e81f5b1440e1866692c45
victorpena13 <victor.pena1392@gmail.com>
2020-03-07 14:24:43 -0600

    Create a file called counter.php using the page controller template.In your HTML, you will need two links. One that says up and another that says down.

---
+++ counter.php
@@ -0,0 +1,29 @@
+<?php
+
+//Require or include statements are allowed here.
+//All other code goes in the pageController function.
+
+function pageController() {
+    //handles all processing for this page.
+
+
+
+
+
+
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>counter</title>
+    </head>
+    <body>
+
+    <a href="">Up</a>
+    <a href="">down</a>
+
+
+
+    </body>
+</html>
